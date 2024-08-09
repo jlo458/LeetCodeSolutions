55e97cd129dcd1bf320c564c634643a3b3b15455
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-08-09 21:30:10 +0100

    Update LongestPalindrome.py

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,25 +1,40 @@
-# In progress
+# My solution to the "Longest Palindromic Substring" task
 
-string = "babad"
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
 
-def findCh(string, ch):
-  return [i for i, ltr in enumerate(string) if ltr == ch]
+        def findCh(s, ch):
+            return [i for i, ltr in enumerate(s) if ltr == ch]
 
-def findPal(string):
-  for _ in range(len(string)):
-    if string == string[::-1]: 
-      return string
+        pals = []
+        for _ in range(len(s)-1):
+            if s == s[::-1]: 
+                pals.append(s)
 
-    let1 = string[0]
-    inds = findCh(string, let1)
-    inds.pop(0)
-    #print(inds)
+            else:
 
-    for _ in range(len(inds)): 
-      lastLetInd = inds[-1]
-      if string[:lastLetInd-1] == string[:lastLetInd-1][::-1]: 
+                let1 = s[0]
+                inds = findCh(s, let1)
+                inds.pop(0)
+                #print(inds)
+            
+                for lastLetInd in inds[::-1]: 
+                    test = s[:lastLetInd+1]
+                    if test == test[::-1]: 
+                        pals.append(test)
+                        break 
+            
+                s = s[1:]
+
+        if pals == []:
+            return s[0] 
+
+        minLength = 0
+        longPal = ''
+        for pal in pals: 
+            if len(pal) > minLength: 
+                minLength = len(pal)
+                longPal = pal 
+
+        return longPal
         
-    
-
-findPal(string)
-  
