d8f3a4617da3adb06d8a12406d759fd438d45977
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-08-20 21:30:18 +0100

    Update maxDistance.py

--- maxDistance.py
+++ maxDistance.py
@@ -1,46 +1,34 @@
-# In progress 
+# My solution to "Maximum Distance in Arrays" problem on Leetcode
 
-arrays = [[1,5],[3,4]]
-
-def maxDistance(arrays): 
-  if len(arrays) == 0: 
-    return 0
-  smallest = arrays[0][-1] + 1
-  #highest = arrays[0][0]
-
-  sList = None 
-  s2List = None
-  #hList = None
-
-  for ind in range(len(arrays)): 
-    if arrays[ind][0] < smallest:
-      s2List = sList
-      smallest = arrays[ind][0]
-      sList = ind 
-
-  highest = arrays[0][0] - 1
-  hList = None
-  h2List = None
-
-  for ind in range(len(arrays)): 
-    if arrays[ind][-1] > highest:
-      h2List = hList
-      highest = arrays[ind][-1]
-      hList = ind
-
-  if hList == sList: 
-    try:
-      opt1 = highest - arrays[s2List][0]
-      opt2 = arrays[h2List][-1] - smallest 
-      if opt1 > opt2: 
-        smallest = arrays[s2List][0]
-
-      else: 
-        highest = arrays[h2List][-1]
-
-    except: 
-      return maxDistance(arrays.pop(hList)) 
-
-  return highest - smallest
-
-print(maxDistance(arrays))
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        if len(arrays) == 0:
+            return 0
+    
+        min_val = float('inf')
+        second_min_val = float('inf')
+        max_val = float('-inf')
+        second_max_val = float('-inf')
+        min_idx = -1
+        max_idx = -1
+        
+        for i, array in enumerate(arrays):
+            if array[0] < min_val:
+                second_min_val = min_val
+                min_val = array[0]
+                min_idx = i
+            elif array[0] < second_min_val:
+                second_min_val = array[0]
+            
+            if array[-1] > max_val:
+                second_max_val = max_val
+                max_val = array[-1]
+                max_idx = i
+            elif array[-1] > second_max_val:
+                second_max_val = array[-1]
+        
+        if min_idx != max_idx:
+            return max_val - min_val
+        else:
+            return max(second_max_val - min_val, max_val - second_min_val)
+        
